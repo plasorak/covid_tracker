3ed9e50205d6903d6d9cc3991912eb75a2cd9b81
Pierre Lasorak <pierre.lasorak@gmail.com>
2020-07-15 10:50:26 +0100

    typo

--- execute_me_daily.sh
+++ execute_me_daily.sh
@@ -4,6 +4,6 @@ cd /home/plasorak/Documents/covid_tracker
 python3 covid_track.py --council "Oxfordshire" --nosplash
 python3 covid_track.py --council "West Sussex" --nosplash
 git add covid_cases_Oxfordshire.png covid_cases_West_Sussex.png
-git commit -m "update for the ${date}"
+git commit -m "update for the $(date)"
 git push origin master
 cd $current
